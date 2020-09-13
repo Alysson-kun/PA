713dedf4cfc17cda51e631ab5ca9a1bdc1a21fe4
Alysson-kun <alyssonyv@gmail.com>
2020-09-13 18:03:53 -0300

    Update Questão01.c

--- Questão01.c
+++ Questão01.c
@@ -2,8 +2,13 @@
 
 int main(){
 
+// Questão 1: Seja o seguinte trecho de programa:
+
 int i=3,j=5; // São criadas duas variáveis i e j, o valor 3 é atríbuido a variável i e o valor 5 é atríbuido a variável j
 int *p, *q; // São definidos 2 ponteiros, p e q
+
+//Determine o valor das seguintes expressões:
+
 p = &i; // o ponteiro p recebe o endereço de i
 q = &j; // o ponteiro q recebe o endereço de j
 p == &i; // verifica se p é igual ao endereço de i. Essa expressão retorna 1, pois ela é verdadeira.
