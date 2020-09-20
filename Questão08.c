5acf7cc6b76019cd2a2255a72f2435943b29ac82
Sthefania <sthefaniafernandes03@gmail.com>
2020-09-20 17:37:13 -0300

    Update Questão08.c

--- Questão08.c
+++ Questão08.c
@@ -1,14 +1,11 @@
 #include<stdio.h>
 
 int main(){
-
-int x,b;
-
-x=1;
-b=x++;
-printf ("%d, %d", x, b);
-
-
-return 0;
+    //O que fazem os seguintes programas em C?
+    int vet[] = {4,9,13};
+    int i;
+    for(i=0;i<3;i++){
+        printf("%d ",*(vet+i));
+    }
 
 }
