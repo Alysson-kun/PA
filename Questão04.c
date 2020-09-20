35ba7d9b7a58e29e01d54c1ee3c3f3522dfb271f
Sthefania <sthefaniafernandes03@gmail.com>
2020-09-20 16:04:34 -0300

    Update Questão04.c

--- Questão04.c
+++ Questão04.c
@@ -20,35 +20,35 @@ foram obtidas as respostas esperadas).*/
   valor = 10;
   p1 = &valor;
   *p1 = 20;
-  printf("%d \n", valor);
+  printf("%d \n", valor); // o ponteiro "p1" recebeu o endereço de "valor", como foi alterado o conteúdo de "p1" o valor da variável "valor" também foi alterado, portanto a saída é 20.
   
   /* (b) */
   temp = 26.5;
   p2 = &temp;
   *p2 = 29.0;
-  printf("%.1f \n", temp);
+  printf("%.1f \n", temp); // o ponteiro "p2" recebeu o endereço de "temp", como foi alterado o conteúdo de "p2" o valor da variável "temp" também foi alterado, portanto a saída é 29.0.
   
   /* (c) */
   p3 = &nome[0];
   aux = *p3;
-  printf("%c \n", aux);
+  printf("%c \n", aux); // o ponteiro "p3" recebeu o endereço guardado na posição 0 do vetor de ponteiros "nome", o char "aux" recebeu o conteúdo de "p3", portanto a saída é "P".
   
   /* (d) */
   p3 = &nome[4];
   aux = *p3;
-  printf("%c \n", aux);
+  printf("%c \n", aux); // o ponteiro "p3" recebeu o endereço guardado na posição 4 do vetor de ponteiros  "nome", o char "aux" recebeu o conteúdo de "p3", portanto a saída é "e".
   
   /* (e) */
   p3 = nome;
-  printf("%c \n", *p3);
-  
+  printf("%c \n", *p3); // Ao fazer p3=nome o comando está atribuindo o primeiro endereço guardado no vetor de ponteiros "nome" a "p3", portanto a saída é "P".
   /* (f) */
+
   p3 = p3 +4;
-  printf("%c \n", *p3);
+  printf("%c \n", *p3); // Como p3 tinha guardado o primeiro endereço do vetor de ponteiros "nome", ao instruir que "p3" recebesse o endereço guardado em p3+4 foi dito que ele caminhasse 4 posições para frente no conjunto de elementos guardado em "nome", portanto temos a saída "e".
   
   /* (g) */
   p3--;
-  printf("%c \n", *p3);
+  printf("%c \n", *p3); // Como p3 tinha guardado o endereço da posição 5 do vetor de ponteiros "nome", ao decrementar uma posição temos como saída "t".
   
   /* (h) */
   vetor[0] = 31;
@@ -56,21 +56,21 @@ foram obtidas as respostas esperadas).*/
   vetor[2] = 27;
   p4 = vetor;
   idade = *p4;
-  printf("%d \n", idade);
+  printf("%d \n", idade); // "p4" vai receber o endereço guardado na posição 0 do "vetor", "idade" vai receber o conteúdo do endereço de "p4" que é 31.
   
   /* (i) */
   p5 = p4 +1;
   idade = *p5;
-  printf("%d \n", idade);
+  printf("%d \n", idade); // "p5" vai receber o endereço de "p4" incrementado mais uma posição, ou seja, a posição 1 do "vetor". "idade" vai receber o conteúdo guardado no endereço armazenado em "p5", portanto a saída é 45.
   
-  /* (j) */
+  /* (j) */ 
   p4 = p5 +1;
   idade = *p4;
-  printf("%d \n", idade);
+  printf("%d \n", idade); // "p5" está com o endereço de vetor[1] guardado. Ao atribuir a "p4" "p5+1" estamos guardando nesse ponteiro o endereço de vetor 2, logo idade recebe 27.
 
   /* (l) */
   p4 = p4 - 2;
   idade = *p4;
-  printf("%d \n", idade);
+  printf("%d \n", idade); // Como "p4" está com o endereço de vetor[2], ao atribuir p4=p4-2 estamos guardando em "p4" o endereço de vetor[0] novamente, logo idade é 31.
 
 }
