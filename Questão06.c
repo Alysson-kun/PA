59f88e4ef9585d0dc66bfb156cda9ea5b267e746
Sthefania <sthefaniafernandes03@gmail.com>
2020-09-20 17:37:17 -0300

    Update Questão06.c

--- Questão06.c
+++ Questão06.c
@@ -1,12 +1,15 @@
 #include<stdio.h>
 
 int main(){
-//Assumindo que pulo[] é um vetor do tipo int, quais das seguintes expressões referenciam o valor do terceiro elemento do vetor?
 
-*(pulo + 2); 
-*(pulo + 4);
-// a operação de deferenciação só pode ser realizada com ponteiros, portanto não pode ser nenhuma das duas primeiras alternativas.
-pulo + 4;
-pulo + 2;
-// A opção "pulo + 4" vai retornar o valor da posição 5 do vetor "pulo", pois pulo corresponde ao endereço do elemento 0 do vetor. Portanto, "pulo + 2" é a alternativa certa.
+// DÚVIDA: a operação de deferenciação não é exclusiva de ponteiros?
+
+//Assumindo que pulo[] é um vetor do tipo int, quais das seguintes expressões referenciam o valor do terceiro elemento do vetor?
+    int pulo[] = {4,9,13};
+    *(pulo + 2); 
+    *(pulo + 4);
+    // A opção "*(pulo + 4)" vai retornar o valor da posição 5 do vetor "pulo", pois "pulo" corresponde ao endereço do elemento 0 do vetor. Portanto, "*(pulo + 2)" é a alternativa certa.
+    pulo + 4;
+    pulo + 2;
+    // A opção "pulo + 4" vai retornar o valor da posição 5 do vetor "pulo", pois pulo corresponde ao endereço do elemento 0 do vetor. Portanto, "pulo + 2" também é uma alternativa certa.
 }
