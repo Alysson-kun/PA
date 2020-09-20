76064bdaf9cc56cbaf5a488dc30bf57d68db36b0
Sthefania <sthefaniafernandes03@gmail.com>
2020-09-20 17:37:21 -0300

    Update Questão04.c

--- Questão04.c
+++ Questão04.c
@@ -5,7 +5,7 @@ int main(){
 /*Determine o que será mostrado pelo seguinte programa (compile-o, execute-o e verifique se
 foram obtidas as respostas esperadas).*/
 
- int valor;
+  int valor;
   int *p1;
   float temp;
   float *p2;
