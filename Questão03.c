17bb5f533e64987556122e736988df8e832ef6e0
Sthefania <sthefaniafernandes03@gmail.com>
2020-09-20 17:37:24 -0300

    Update Questão03.c

--- Questão03.c
+++ Questão03.c
@@ -1,18 +1,18 @@
 #include<stdio.h>
 
 int main(){
-
+// DÍVIDA: não entendi i = *&*&j;
 // Questão 3: Se i e j são variáveis inteiras e p e q ponteiros para int, quais das seguintes expressões de atribuição são ilegais?
 
-int i, j;
-int *p, *q;
+    int i, j;
+    int *p, *q;
 
-p = i; // está incorreto, pois um ponteiro só pode atribuir endereços.
-q = &j; // ok
-p = &*&i; // o conteúdo do endereço de i é igual ao endereço de i, logo o endereço do endereço de i não faz sentido.
-i = (*&)j; // *& retorna o conteúdo do endereço de nada
-i = *&j; // ok
-i = *&*&j; // n sei ainda
-q = *p; // está incorreto, pois um ponteiro só pode atribuir endereços.
-i = (*p)++ + *q; // ok
+    p = i; // está incorreto, pois um ponteiro só pode atribuir endereços.
+    q = &j; // ok
+    p = &*&i; // o conteúdo do endereço de i é igual ao endereço de i, logo o endereço do endereço de i não faz sentido.
+    i = (*&)j; // *& retorna o conteúdo do endereço de nada
+    i = *&j; // ok
+    i = *&*&j; // n sei ainda
+    q = *p; // está incorreto, pois um ponteiro só pode atribuir endereços.
+    i = (*p)++ + *q; // ok
 }
