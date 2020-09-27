331627e680124831e79e4805b962c2e431bba169
Sthefania <sthefaniafernandes03@gmail.com>
2020-09-27 15:52:21 -0300

    Update Questão08b.c

--- Questão08b.c
+++ Questão08b.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
 
 int main(){
-    //b
+    //b)
     int vet[] = {4,9,13};
     int i;
     for(i=0;i<3;i++){
-        printf("%X ",vet+i);
+        printf("%X ",vet+i); // imprime o endereço de "vet" na posição 0, incrementando 1 a esse valor até i<3. Alterando o endereço que irá ser exibido de acordo com o valor de i.
     }
+    //Esse programa imprime os endereços armazenados no array vet.
 }
