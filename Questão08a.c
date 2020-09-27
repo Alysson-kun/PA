79d01bcd9b5d28653d2a5bd77aa0f69b61ad8235
Sthefania <sthefaniafernandes03@gmail.com>
2020-09-27 15:52:26 -0300

    Update Questão08a.c

--- Questão08a.c
+++ Questão08a.c
@@ -1,13 +1,12 @@
 #include<stdio.h>
 
 int main(){
-    // continua...
     //O que fazem os seguintes programas em C?
     // a)
     int vet[] = {4,9,13}; // define um vetor 
     int i; // define uma variável
     for(i=0;i<3;i++){
-        printf("%d ",*(vet+i)); // imprime o conteúdo de "vet" na posição 0, incrementando a essa posição a ser exibida de acordo com o valor de i.
+        printf("%d ",*(vet+i)); // imprime o conteúdo de "vet" na posição 0, incrementando 1 a esse valor até i<3. Alterando a posição que irá ser exibida de acordo com o valor de i.
     }
-
+    //Esse programa imprime o conteúdo de um vetor.
 }
