9b897bdc5f89387432f64bc419322d2a05897f9b
Sthefania <sthefaniafernandes03@gmail.com>
2020-10-10 16:31:32 -0300

    Create Questão15.c

---
+++ Questão15.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main (void){
+    
+    //Reimplemente o programa da questão anterior utilizando a função qsort() do C. Comente o seu código, explicando o que faz cada uma das linhas.
+    
+
+}
