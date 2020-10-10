e5ffaf4b12511860401473d2c8aba0ddb9d82664
Sthefania <sthefaniafernandes03@gmail.com>
2020-10-10 16:02:38 -0300

    Create Questão13.c

---
+++ Questão13.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+//Implemente em linguagem C uma função em um programa de computador que leia n valores do tipo float e os apresente em ordem crescente. Utilize alocação dinâmica de memória para realizar a tarefa.
+
+    float *p, m;
+    int n;
+    printf ("tamanho do vetor: ");
+    scanf ("%f", m);
+    p = malloc(m*sizeof(float));
+    p[1]=2.5;
+    printf ("%f \n", p[1]);
+
+    /*printf ("preencha o vetor: ");
+    for (int i=0; i<n; i++)
+        scanf ("%f \n", p[i]);
+
+    for (int j=0; j<n; j++)
+    {
+        int aux=0;
+        if (p[j]>p[j+1])
+        {
+            aux=p[j];
+            p[j]=p[j+1];
+            p[j+1]=aux;
+        }
+
+    }
+    printf ("vetor ordenado: ");
+    for (int i=0; i<n; i++)
+        printf ("%f \n", p[i]);*/
+
+
+
+}
