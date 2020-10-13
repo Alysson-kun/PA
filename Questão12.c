366655a48ee8074c5a75e83f28d843d2a6903b08
Alysson-kun <alyssonyv@gmail.com>
2020-10-12 23:08:39 -0300

    Update Questão12.c

---
+++ Questão12.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* O que é um ponteiro para uma função? Pesquise na Internet referências sobre o assunto e
+escreva um pequeno programa exemplificando o uso deste recurso */
+
+/*Resposta: Segundo Pinho (201x), uma função é um aglomerado de comandos armazenado em um bloco, cujo qual, pode ser acessado através de um nome. Sendo assim, sabendo que um ponteiro dá acesso a um edereço através de um nome, um ponteiro para função guardaria o endereço de uma função na memória. Ou seja, ponteiro para função é um meio de acessar um conjunto de instruções guardadas em um lugar na memoria. */
+
+int multiplicacao (int n1, int n2)
+{
+    return n1*n2;
+}
+
+int main ()
+{
+    int (*px) (int, int)=multiplicacao;
+
+    int a;
+    int b;
+
+    printf("Digite o primeiro numero: ");
+    scanf("%d",&a);
+    printf("Digite o segundo numero: ");
+    scanf("%d",&b);
+
+    printf("O resultado eh: %d", (*px)(a,b));
+}
